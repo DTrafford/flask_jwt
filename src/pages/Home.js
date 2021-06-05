9ca4fcbd27228498fac0fb79454f485264623f9e
David Trafford <d.trafford@outlook.com>
2021-06-05 15:19:16 -0400

    set up

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React, {useEffect} from 'react';
+
+export default function Home() {
+    useEffect(() => {
+      fetch("/api").then(resp => resp.json()).then(resp => console.log(resp))
+    }, [])
+    return <h2>Home</h2>;
+  }
